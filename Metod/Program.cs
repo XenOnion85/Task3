15e291bb8799ccf6740a2f73b17ed25dd58b67bb
XenOnion85 <100402573+XenOnion85@users.noreply.github.com>
2022-03-03 21:17:36 +0300

    Add files via upload

---
+++ Metod/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Metod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число a: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Введите число b: ");
+            int b = int.Parse(Console.ReadLine());
+            Utils ut = new Utils();
+            Console.WriteLine("Максимальное число: " + ut.Greater(a, b));
+        }
+    }
+}
---
+++ Metod/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metod
+{
+    class Utils
+    {
+        public int Greater(int a, int b)
+        {
+            return Math.Max(a, b);
+        }
+    }
+}
